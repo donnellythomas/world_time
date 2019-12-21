cef5ca7a9e6ce7043e27f7e7061abe7f3c517614
Thomas Donnelly <donnelly.thomas21@gmail.com>
2019-12-21 14:59:02 -0500

    added custom worldtime class with loading screen

--- lib/pages/choose_location.dart
+++ lib/pages/choose_location.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:http/http.dart';
-import 'dart:convert';
 
 class ChooseLocation extends StatefulWidget {
   @override
@@ -8,28 +6,10 @@ class ChooseLocation extends StatefulWidget {
 }
 
 class _ChooseLocationState extends State<ChooseLocation> {
-  void getData() async {
-    Response response =
-        await get('https://jsonplaceholder.typicode.com/todos/1');
-    Map data = jsonDecode(response.body);
-    print(data['title']);
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    getData();
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.grey[200],
-      appBar: AppBar(
-        backgroundColor: Colors.blue[900],
-        centerTitle: true,
-        elevation: 0,
-      ),
     );
   }
 }
